f6f8f66ab4b791e0f4645bd21eb319cfdd2ec80f
rebeljel <s83069@beuth-hochschule.de>
2022-01-24 18:50:09 +0100

    main pull

--- zcrpts_settings/settings.py
+++ zcrpts_settings/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -25,8 +24,19 @@ SECRET_KEY = 'django-insecure-4g^&m+zt0*qja$snmm*olv32x%b7-u0xi$1ufm6u#(0xmllh&=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Deployment
 
+# SECRET_KEY
+# with open('/etc/secret_key.txt') as f:
+#    SECRET_KEY = f.read().strip()
+
+# DEBUG = False
+
+# CSRF_COOKIE_SECURE = True
+
+# SESSION_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -73,7 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'zcrpts_settings.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -83,7 +92,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -103,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     # },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -116,7 +123,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
